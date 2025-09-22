6851296b54c418866fda74981f93d9a96a27c86b
Acc-waelosman <waos0887@gmail.com>
2025-09-22 14:50:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -35,8 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
     "رئيس حسابات",
     "مدير مالي",
     "محلل مالي",
-    "مراجع حسابات",
-    "وائل عثمان"
+    "مراجع حسابات"
   ];
   let professionIndex = 0;
   let charIndex = 0;
@@ -54,16 +53,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     if (!isDeleting && charIndex === currentProfession.length + 1) {
-      setTimeout(() => isDeleting = true, 1500); // Wait 1.5 seconds before deleting
+      setTimeout(() => isDeleting = true, 1000); // انتظر ثانية قبل البدء في المسح
     } else if (isDeleting && charIndex === 0) {
       isDeleting = false;
       professionIndex = (professionIndex + 1) % professions.length;
-      setTimeout(typeProfession, 500); // Wait 0.5 seconds before typing the next word
+      setTimeout(typeProfession, 500); // انتظر نصف ثانية قبل كتابة الكلمة التالية
     } else {
-      const typingSpeed = isDeleting ? 75 : 150; // Deleting is faster than typing
+      const typingSpeed = isDeleting ? 75 : 150; // سرعة المسح أسرع من الكتابة
       setTimeout(typeProfession, typingSpeed);
     }
   }
 
-  typeProfession(); // Start the typing function
+  typeProfession(); // ابدأ وظيفة الكتابة
 });
